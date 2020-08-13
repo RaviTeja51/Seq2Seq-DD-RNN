638e7b7c5cdbbd3e8ebb8fd49ef80adfc223cab7
RaviTeja51 <holyindus08@gmail.com>
2020-08-13 22:13:00 +0530

    changed attention from dot to multiplicative

--- attention.py
+++ attention.py
@@ -1,42 +1,32 @@
 import tensorflow as tf
-from tensorflow.keras import layers
+from tensorflow.keras import layers, Model
 
 ## implementation reference https://blog.floydhub.com/attention-mechanism/
-## https://github.com/uzaymacar/attention-mechanisms/blob/master/layers.py
-class LuongAttention(layers.Layer):
+## https://machinetalk.org/2019/03/29/neural-machine-translation-with-attention-mechanism/
+
+class LuongAttention(Model):
+
     def __init__(self, hidden_size, method="dot"):
         super(LuongAttention, self).__init__()
         self.method = method
         self.hidden_size = hidden_size
 
-        if method == "general":
-            self.fc = layers.Dense(hidden_size,use_bias=False)
-
-        elif method == "concat":
-            self.fc = layers.Dense(hidden_size, use_bias=False)
-            self.v_a = Dense(units=1, use_bias=False)
+        self.wa = layers.Dense(hidden_size)
 
     def __call__(self, decoder_hidden, encoder_output):
-        # encoder_output shape (BATCH_SIZE, TIME STEP, UNITS)
-        # decoder_hidden shape (BATCH_SIZE, UNITS)
+        #decoder_hidden shape (batch_size,units)
+        # encoder_outputs shape(batch_size, time_steps, units)
 
-        # add time axis to decoder_hidden h_t
-        decoder_hidden = tf.expand_dims(decoder_hidden,axis=1)
+        decoder_hidden = tf.expand_dims(decoder_hidden,1) #decoder_hidden shape (batch_size, 1,units)
 
-        if self.method == "dot":
-            score = layers.Dot([2,2])([encoder_output, decoder_hidden]) #SHAPE (BATCH_SIZE, TIME STEP, 1)
+        score = tf.matmul(decoder_hidden, self.wa(encoder_output), transpose_b=True)
+        # score will have shape: (batch_size, 1, max_len)
 
-        elif self.method == "general":
-            weighted_encoder_output =  self.fc(encoder_output)
-            score =  layers.Dot([2,2])([weighted_encoder_output, decoder_hidden])  #SHAPE (BATCH_SIZE, TIME STEP, 1)
+        # alignment vector a_t
+        alignment = tf.nn.softmax(score, axis=2)
 
-        elif self.method == "concat":
-            weighted_sum = weighted_encoder_output  + weighted_decoder_hidden  #SHAPE(BATCH_SIZE, TIME STEP, UNITS)
-            weighted_sum = self.fc(weighted_sum) #SHAPE(BATCH_SIZE, TIME STEP, UNITS)
-            weighted_sum =  layers.Activation("tanh")(weighted_sum) #SHAPE (BATCH_SIZE, TIME STEP, UNITS)
-            score = self.v_a(weighted_sum)   #SHAPE (BATCH_SIZE, TIME STEP, 1)
+        # context vector c_t is the average sum of encoder output
+        context = tf.matmul(alignment, encoder_output)
+        #  vector's shape: (batch_size, 1, rnn_size)
 
-        attention_weights = tf.nn.softmax(score, axis = 2) #SHAPE (BATCH_SIZE, TIME STEP, 1)
-
-        context_vector = tf.matmul(attention_weights, encoder_output) #SHAPE (BATCH_SIZE, TIME STEP, UNITS)
-        return attention_weights, context_vector
+        return context, alignment
